b11e71495d2476c8830ccf4c0ba8e5ad1df33965
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-09-17 17:51:24 -0300

    feat: add Products component for displaying product information

---
+++ frontend/src/pages/Products.tsx
@@ -0,0 +1,9 @@
+export default function Products() {
+    return (
+        <main className="min-w-0 flex-1 p-8">
+            <h2 className="text-3xl font-bold text-slate-900">
+            Produtos
+            </h2>
+        </main>
+    )
+}
