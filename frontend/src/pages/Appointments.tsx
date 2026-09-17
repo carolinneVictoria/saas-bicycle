e072eb1c1252455fd0784086808ad0744e8c2388
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-09-17 17:51:13 -0300

    feat: add Appointments component for displaying agenda

---
+++ frontend/src/pages/Appointments.tsx
@@ -0,0 +1,9 @@
+export default function Appointments() {
+    return (
+        <main className="min-w-0 flex-1 p-8">
+            <h2 className="text-3xl font-bold text-slate-900">
+            Agenda
+            </h2>
+        </main>
+    )
+}
