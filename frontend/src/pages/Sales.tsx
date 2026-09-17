88dd47f6ff2ddfd829eb69f8d21286e2a631c44b
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-09-17 17:51:32 -0300

    feat: add Sales component for displaying sales information

---
+++ frontend/src/pages/Sales.tsx
@@ -0,0 +1,9 @@
+export default function Sales() {
+    return (
+        <main className="min-w-0 flex-1 p-8">
+            <h2 className="text-3xl font-bold text-slate-900">
+            Vendas
+            </h2>
+        </main>
+    )
+}
